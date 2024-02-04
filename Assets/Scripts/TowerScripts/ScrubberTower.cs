496d749d567d67eebc88b77eff1092ea400751d1
Nicko-Kingo <knick98@vt.edu>
2024-02-04 01:41:49 -0500

    Implemented ScrubberTower
    Stops the smog enemies and attacks everything in front of it

--- Assets/Scripts/TowerScripts/ScrubberTower.cs
+++ Assets/Scripts/TowerScripts/ScrubberTower.cs
@@ -1,18 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class ScrubberTower : MonoBehaviour
+public class ScrubberTower : TowerControl
 {
+    //where to keep the smogs
+    private Transform pos;
+
+    //smogs in the thing
+    private List<SmogEnemy> smogs;
+
     // Start is called before the first frame update
     void Start()
     {
+        SetSpeed(3);
+        SetDamage(12);
+        SetHealth(20);
+        StartCoroutine(AttackCycle());
+
+        smogs = new List<SmogEnemy>();
+    }
+
+    private IEnumerator AttackCycle()
+    {
+        while(true)
+        {
+            Attack();
+            yield return new WaitForSeconds(GetSpeed());
+        }
+
+        
+    }
+
+    public override void Attack()
+    {
+        Debug.Log("Attacking");
+        RaycastHit[] hits;
+        int hitMask = 1 << 6;
+        hits = Physics.RaycastAll(transform.position + Vector3.left, transform.right, 2.0f, hitMask);
+        
+        Debug.DrawRay(transform.position + Vector3.left, Vector3.right);
+
+
+        for (int i = 0; i < hits.Length; i++) {
+            
+            RaycastHit hit = hits[i];
+            GameObject target = hit.collider.gameObject;
+            //if (target == null) continue;
+            Enemy enemy = target.GetComponent<Enemy>();
+
+            if(enemy != null)
+            {
+                Debug.Log("Hit something??");
+                enemy.Damage(GetDamage());
+            }
+        }
+
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //Hold them at this location, until this tower dies
+        for(int i = 0; i < smogs.Count; i++)
+        {
+            if(smogs[i] == null)
+            {
+                smogs.Remove(smogs[i]);
+                continue;
+            }
+            smogs[i].transform.position = pos.position;
+        }
     }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collided with a thing!");
+        if(collision.gameObject.GetComponent<SmogEnemy>() != null)
+        {
+            smogs.Add(collision.gameObject.GetComponent<SmogEnemy>());
+            if(pos == null)
+            {
+                pos = collision.transform;
+            }
+        }
+
+    }
+
+
+
+
 }
