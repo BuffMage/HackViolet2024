0d5f3a8eb8cb00eb1db70a0b9e44bb4a453d0c5e
Nicko-Kingo <knick98@vt.edu>
2024-02-04 02:26:28 -0500

    Debuff manager
    More work needs to be done but yeah. See comments

--- Assets/Scripts/EnemyScripts/DebuffManager.cs
+++ Assets/Scripts/EnemyScripts/DebuffManager.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 
 public class DebuffManager : MonoBehaviour
 {
-    //Tower dictionary
+    //REDO THIS SONNNNNNNNN
+    //Make the dicitonary itself ambiguous, then
+    //figure it out from there
     private Dictionary<TowerControl, float> affectedTowers;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        affectedTowers = new Dictionary<TowerControl, float>();
     }
 
     // Update is called once per frame
@@ -24,11 +26,16 @@ public class DebuffManager : MonoBehaviour
     //for whatever reason
     public void ApplyDebuff(GameObject Target, float time)
     {
+        Debug.Log("DEBUFF APPLIED");
         if(Target.GetComponent<TowerControl>() != null)
         {
-            if(affectedTowers.Keys.Contains(Target.GetComponent<TowerControl>()))
+
+            TowerControl te = Target.GetComponent<TowerControl>();
+            if(affectedTowers.ContainsKey(te))
             {
                 Debug.Log("Tower Found!");
+                
+                affectedTowers[Target.GetComponent<TowerControl>()] = time;
             }
         }
         else if(Target.GetComponent<Enemy>() != null)
@@ -37,5 +44,45 @@ public class DebuffManager : MonoBehaviour
         }
     }
 
+    private void StartDebuff(TowerControl tower)
+    {
+        tower.SetDamage(tower.GetDamage() / 2);
+        tower.SetSpeed(tower.GetSpeed() * 2);
+    }
+
+    private void StartDebuff(Enemy enemy)
+    {
+        //Unimplemented
+    }
+
+    private void EndBuff(TowerControl tower)
+    {
+        affectedTowers.Remove(tower);
+        tower.SetDamage(tower.GetDamage() * 2);
+        tower.SetSpeed(tower.GetSpeed() / 2);
+    }
+
+    private void EndBuff(Enemy enemy)
+    {
+        //Unimplemented
+    }
+
+    //Every second subtract by 1??
+    private IEnumerator DebuffCycle()
+    {
+        while(true)
+        {
+            List<TowerControl> keys = affectedTowers.Keys.ToList();
+            for(int i = 0; i < keys.Count; i++)
+            {
+                affectedTowers[keys[i]] -= 1;
+                if(affectedTowers[keys[i]] < .01f)
+                {
+                    EndBuff(keys[i]);
+                }
+            }
+            yield return new WaitForSeconds(1f);
+        }
+    }
 
 }
